e6ded5890da8f8e4eb61659a2f3a805dda9258bf
javs1 <javierverduzco13@gmail.com>
2023-12-11 19:31:38 -0500

    Primeros avanzes, inicializando el tablero

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+var canvas;
+var ctx;
+var fps = 1;
+
+var canvasX = 500;  //pixels ancho
+var canvasY = 500;  //pixels alto
+var tileX, tileY;
+
+//Iniciar el tablero
+var tablero;
+var filas = parseInt(prompt('Introduce las filas'));
+var columnas = parseInt(prompt('Introduce las columnas'));
+
+var blanco = '#FFFFFF';
+var verde = '#008000';
+
