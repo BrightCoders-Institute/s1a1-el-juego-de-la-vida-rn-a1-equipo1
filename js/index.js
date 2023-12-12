5305f937a06979182dcea43cfd30389a81dd698f
javs1 <javierverduzco13@gmail.com>
2023-12-11 20:29:14 -0500

    Creating the Agents and caracteristics
    Co-authored-by: YahirCl <YahirCl@users.noreply.github.com>
    Co-authored-by: GAAF <FelixXD2234@users.noreply.github.com>
    Co-authored-by: Missael Peralta <Missael-Peralta@users.noreply.github.com>

--- js/index.js
+++ js/index.js
@@ -15,3 +15,61 @@ var columnas = parseInt(prompt('Introduce las columnas'));
 var blanco = '#FFFFFF';
 var verde = '#008000';
 
+
+
+function creaArray2D(f,c){
+    var obj = new Array(f);
+    for(y=0; y<f; y++){
+        obj[y]= new Array(c);
+    }
+
+    return obj;
+}
+
+
+//Objeto Agente o Turmita
+var Agente = function(x, y, estado){
+    this.x = x;
+    this.y = y;
+    this.estado = estado;
+    this.estadoProx = this.estado; 
+    
+    this.vecinos = []; //Vecinos del agente
+
+    this.addVecinos = function(){
+        
+    }
+}
+
+
+function inicializa(){
+
+    //Asiociamos el canvas
+    canvas = Document.getElementById('pantalla');
+    ctx = canvas.getContext('2D');
+
+    //Ajustamos el tamaño
+    canvas.width = canvasX;
+    canvas.height = canvasY;
+
+    //calcular los tamaños de los tiles
+
+    tileX = Math.floor(canvasX/filas);
+    tileY = Math.floor(canvasY/columnas);
+
+    //Creamos el tablero
+    tablero = creaArray2D(filas, columnas);
+
+    //Ejecutamos el bucle principal
+    setInterval(function(){principal();},1000/fps);
+}
+
+function borrarCanvas(){
+    canvas.width  = canvas.width;
+    canvas.height = canvas.height;
+}
+
+function principal(){
+    console.log('fotograma');
+    borrarCanvas();   
+}
