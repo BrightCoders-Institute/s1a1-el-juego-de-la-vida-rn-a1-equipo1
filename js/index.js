49046c760035e40edadba0458c3d0a1af90af3e4
javs1 <javierverduzco13@gmail.com>
2023-12-12 20:35:00 -0500

    Starting with ESLint
    Co-authored-by: FelixXD2234 <felavalosg@gmail.com>
    Co-authored-by: WEROSKI <oheredia2@ucol.mxl@users.norep>
    Co-authored-by: Missael-Peralta <rperalta0@ucol.mx>
    Co-authored-by: YahirCl <ycortes1@ucol.mx>

--- js/index.js
+++ js/index.js
@@ -1,105 +1,109 @@
+let canvas;
+let ctx;
+const fps = 1;
 
-var canvas;
-var ctx;
-var fps = 1;
+const canvasX = 500; // pixels ancho
+const canvasY = 500; // pixels alto
+let tileX;
+let tileY;
 
-var canvasX = 500;  //pixels ancho
-var canvasY = 500;  //pixels alto
-var tileX, tileY;
+// Iniciar el tablero
+let tablero;
+const filas = parseInt(prompt('Introduce las filas'));
+const columnas = parseInt(prompt('Introduce las columnas'));
 
-//Iniciar el tablero
-var tablero;
-var filas = parseInt(prompt('Introduce las filas'));
-var columnas = parseInt(prompt('Introduce las columnas'));
+const blanco = '#FFFFFF';
+const verde = '#008000';
 
-var blanco = '#FFFFFF';
-var verde = '#008000';
-
-
-
-function creaArray2D(f,c){
-    var obj = new Array(f);
-    for(let y=0; y<f; y++){
-        obj[y] = new Array(c).fill(0);
-    }
-
-    return obj;
+// expected @param tag for parameter num1 but found num instead
+// missing @param tag for parameter num2
+// missing return type
+/**
+ * Add two numbers.
+ * @param {number} f The first number.
+ * @param {number} c The first number.
+ * @returns obj The sum of the two numbers.
+ */
+function creaArray2D(f,c) {
+  const obj = new Array(f);
+  for (let y=0; y<f; y++) {
+    obj[y] = new Array(c).fill(0);
+  }
+  
+  return obj;
 }
 
+// Objeto Agente o Turmita
+const Agente = function(x, y, estado) {
+  this.x = x;
+  this.y = y;
+  this.estado = estado;
+  this.estadoProx = this.estado;
+  this.vecinos = []; // Vecinos del agente
 
-//Objeto Agente o Turmita
-var Agente = function(x, y, estado){
-    this.x = x;
-    this.y = y;
-    this.estado = estado;
-    this.estadoProx = this.estado; 
-    
-    this.vecinos = []; //Vecinos del agente
+  this.addVecinos = function() {
+    let xVecino;
+    let yVecino;
 
-    this.addVecinos = function(){
-        var xVecino;
-        var yVecino;
+    for (i = -1; i < 2; i++) {
+      for (j = -1; j < 2; j++) {
+        xVecino = (this.x + j + columnas) % columnas;
+        yVecino = (this.y + i + filas) % filas;
 
-        for (i = -1; i < 2; i++) {
-            for (j = -1; j < 2; j++) {
-                xVecino = (this.x + j + columnas) % columnas;
-                yVecino = (this.y + i + filas) % filas;
-
-                // Se descarta la celula actual
-                if (i!=0 || j!=0) {
-                    this.vecinos.push(tablero[yVecino][xVecino])
-                }
-            }
+        // Se descarta la celula actual
+        if (i!=0 || j!=0) {
+          this.vecinos.push(tablero[yVecino][xVecino]);
         }
+      }
+    }
+  };
+
+  this.dibuja = function() {
+    let color;
+
+    if (this.estado == 1) {
+      color = blanco;
+    } else {
+      color = verde;
     }
 
-    this.dibuja = function(){
-        var color; 
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x * tileX, this.y * tileY, tileX, tileY);
+  };
 
-        if(this.estado ==  1){
-            color =  blanco; 
-        }
-        else {
-            color =  verde; 
-        }
-        ctx.fillStyle = color ;
-        ctx.fillRect (this.x * tileX, this.y * tileY, tileX, tileY); 
+  // programamos las leyes de coway
+
+  this.nuevoCiclo = function() {
+    let suma = 0;
+
+    // calculamos la cantidad de vecinos vivos
+
+    for ( i = 0; i< this.vecinos.length; i++) {
+      suma += this.vecinos[i].estado;
     }
 
-    // programamos las leyes de coway 
+    // Aplicamos las normas
 
-    this.nuevoCiclo  =  function () {
-        var suma =  0; 
+    this.estadoProx = this.estado; // por defecto lo dejamos igual
 
-        // calculamos la cantidad de vecinos vivos 
-        
-        for ( i = 0; i< this.vecinos.length; i++){
-            suma += this.vecinos[i].estado;
-        }
+    // Muerte: tiene menos de 2 o mas de tres
 
-        //Aplicamos las normas
-        
-        this.estadoProx = this.estado; // por defecto lo dejamos igual
+    if (suma <2 || suma> 3 ) {
+      this.estadoProx = 0;
+    }
+    //  VIDA/ REPRODUCCION: Tiene 3 vecinos
 
-        //Muerte: tiene menos de 2 o mas de tres 
-
-        if ( suma <2 || suma> 3 ){
-            this.estadoProx =  0;
-
-        }
-      //  VIDA/ REPRODUCCION: Tiene 3 vecinos 
-
-        if(suma == 3 ){
-            this.estadoProx =  1; 
-        }
-    } 
+    if (suma == 3) {
+      this.estadoProx = 1;
+    }
+  };
 
     this.mutacion =  function() {
-        this.estado = this.estadoProx; 
+        this.estado = this.estadoProx;
     }
 }
 
-function inicializaTablero(obj) {
+function inicializaTablero(obj){
     var estado;
 
     for (y = 0; y < filas; y++) {
